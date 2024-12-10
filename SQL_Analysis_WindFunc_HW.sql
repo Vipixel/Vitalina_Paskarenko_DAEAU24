cf2ebc1657810ee7d272031a5fbffda5a567bb34
Vitalina <66271158+Vipixel@users.noreply.github.com>
2024-12-11 00:05:01 +0100

    SQL_Analysis_WindFunc_HW

---
+++ SQL_Analysis_WindFunc_HW.sql
@@ -0,0 +1,95 @@
+-----Task 1
+SELECT 
+	channel_desc,
+    cust_last_name,
+    cust_first_name,
+	sales$ as amount_sold,
+	sales_percentage
+	row_number
+FROM (
+	SELECT
+	channel_desc,
+    cust.cust_last_name,
+    cust.cust_first_name,
+    TO_CHAR(SUM(s.amount_sold), '9,999,999.99') AS sales$,
+    ROUND((SUM(s.amount_sold) * 10000000.0 / SUM(SUM(s.amount_sold)) OVER (PARTITION BY ch.channel_desc))) || '%' AS sales_percentage,
+    ROW_NUMBER() OVER (PARTITION BY channel_desc ORDER BY SUM( s.amount_sold) DESC) AS row_number
+FROM sh.sales s
+JOIN sh.customers cust ON s.cust_id = cust.cust_id
+JOIN sh.channels ch ON s.channel_id = ch.channel_id
+GROUP BY 
+    ch.channel_desc, 
+    cust.cust_last_name, 
+    cust.cust_first_name
+) ranked_customers
+WHERE row_number IN (1, 2, 3, 4, 5)
+ORDER BY channel_desc, row_number;
+
+----Task 2
+
+SELECT 
+    p.prod_name AS product_name,
+    ROUND(SUM(CASE WHEN t.calendar_quarter_number = 1 THEN s.amount_sold ELSE 0 END), 2) AS q1,
+    ROUND(SUM(CASE WHEN t.calendar_quarter_number = 2 THEN s.amount_sold ELSE 0 END), 2) AS q2,
+    ROUND(SUM(CASE WHEN t.calendar_quarter_number = 3 THEN s.amount_sold ELSE 0 END), 2) AS q3,
+    ROUND(SUM(CASE WHEN t.calendar_quarter_number = 4 THEN s.amount_sold ELSE 0 END), 2) AS q4,
+    ROUND(SUM(CASE WHEN t.calendar_quarter_number IN (1, 2, 3, 4) THEN s.amount_sold ELSE 0 END), 2) AS year_sum
+FROM sh.sales s
+JOIN sh.products p ON p.prod_id = s.prod_id
+JOIN sh.customers cust ON cust.cust_id = s.cust_id
+JOIN sh.times t ON t.time_id = s.time_id
+JOIN sh.channels ch ON ch.channel_id = s.channel_id
+JOIN sh.countries cn ON cn.country_id = cust.country_id
+WHERE 
+    p.prod_category = 'Photo'
+	AND cn.country_region = 'Asia'
+    AND t.calendar_year = 2000
+GROUP BY product_name
+ORDER BY year_sum DESC;
+
+----Task3 
+
+SELECT 
+    channel_desc,
+    cust_id,
+    cust_last_name,
+    cust_first_name,
+    total_sales,
+    row_number
+FROM (
+    SELECT 
+        ch.channel_desc,
+        cust.cust_id,
+        cust.cust_last_name,
+        cust.cust_first_name,
+        ROUND(SUM(s.amount_sold), 2) AS total_sales, 
+        ROW_NUMBER() OVER (ORDER BY  SUM(s.amount_sold) DESC) AS row_number
+    FROM sh.sales s
+    JOIN sh.customers cust ON s.cust_id = cust.cust_id
+    JOIN sh.channels ch ON s.channel_id = ch.channel_id
+    JOIN sh.times t ON s.time_id = t.time_id
+    WHERE t.calendar_year IN (1998, 1999, 2001)
+    GROUP BY 
+        ch.channel_desc,
+        cust.cust_id,
+        cust.cust_last_name,
+        cust.cust_first_name
+) ranked_customers
+WHERE row_number <= 300 
+ORDER BY row_number;
+
+---Task 4
+
+SELECT 
+    TO_CHAR(s.time_id, 'YYYY-MM') AS calendar_month_desc,
+    p.prod_category,
+    SUM(CASE WHEN c.country_region = 'Americas' THEN s.amount_sold ELSE 0 END) AS americas_sales,
+    SUM(CASE WHEN c.country_region = 'Europe' THEN s.amount_sold ELSE 0 END) AS europe_sales
+FROM sh.sales s
+JOIN sh.products p ON s.prod_id = p.prod_id
+JOIN sh.customers cust ON s.cust_id = cust.cust_id
+JOIN sh.countries c ON cust.country_id = c.country_id
+WHERE c.country_region IN ('Europe', 'Americas')
+    AND s.time_id BETWEEN '2000-01-01' AND '2000-03-31'
+GROUP BY TO_CHAR(s.time_id, 'YYYY-MM'), p.prod_category
+ORDER BY TO_CHAR(s.time_id, 'YYYY-MM'), p.prod_category;
