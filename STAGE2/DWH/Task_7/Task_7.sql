8e34c3e4222f9e68ab5ba41c3e5f81e9211c0435
Vitalina <66271158+Vipixel@users.noreply.github.com>
2025-02-20 12:31:41 +0100

    Update task

--- STAGE2/DWH/Task_7/Task_7.sql
+++ STAGE2/DWH/Task_7/Task_7.sql
@@ -1,4 +1,4 @@
--- Begin the transaction
+-- Start DDL transaction
 BEGIN;
 
 -- Create the BL_DM schema if it does not already exist
@@ -10,7 +10,7 @@ CREATE SCHEMA IF NOT EXISTS BL_DM;
 -- This table stores customer information, including historical changes (SCD Type 2)
 
 CREATE TABLE IF NOT EXISTS BL_DM.DIM_CUSTOMER_SCD (
-    customer_surr_id  BIGINT      PRIMARY KEY,  ---- surrogate key
+    customer_surr_id  BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,   ---- surrogate key
     customer_id       BIGINT,  ---- natural key
     customer_src_id   VARCHAR(100),
     customer_name     VARCHAR(50),
@@ -25,48 +25,12 @@ CREATE TABLE IF NOT EXISTS BL_DM.DIM_CUSTOMER_SCD (
     update_dt         DATE
 );
 
--- Insert default row 
-INSERT INTO BL_DM.DIM_CUSTOMER_SCD (
-    customer_surr_id,
-    customer_id,
-    customer_src_id,
-    customer_name,
-    customer_age,
-    customer_level,
-    gender,
-    customer_income,
-    start_dt,
-    end_dt,
-    is_active,
-    insert_dt,
-    update_dt
-)
-SELECT
-    0,
-    -1,
-    'UNKNOWN',
-    'Unknown',
-    -1,
-    'Unknown',
-    'U',
-    -1,
-    '1900-01-01',
-    '1900-01-01',
-    'N',
-    CURRENT_DATE,
-    CURRENT_DATE
-WHERE NOT EXISTS (
-    SELECT 1 FROM BL_DM.DIM_CUSTOMER_SCD
-    WHERE customer_surr_id = 0
-);
-
-
 ------------------------------------------------------------------------------
 -- DIM_PRODUCT_SUPPLIER
 -- This table stores cproduct details and supplier information
 
 CREATE TABLE IF NOT EXISTS BL_DM.DIM_PRODUCT_SUPPLIER (
-    product_surr_id   BIGINT      PRIMARY KEY, ---- surrogate key
+    product_surr_id   BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  ---- surrogate key
     product_id        BIGINT,  ---- natural key
     product_src_id    VARCHAR(100),
     product_name      VARCHAR(50),
@@ -80,48 +44,12 @@ CREATE TABLE IF NOT EXISTS BL_DM.DIM_PRODUCT_SUPPLIER (
     update_dt         DATE
 );
 
--- Default row
------In this row represents an "Unknown" product and is used to handle cases where product data is missing
-INSERT INTO BL_DM.DIM_PRODUCT_SUPPLIER (
-    product_surr_id,  ---- surrogate key
-    product_id,  ---- natural key
-    product_src_id,
-    product_name,
-    category_id,
-    category_name,
-    unit_price,
-    supplier_id,
-    supplier_src_id,
-    supplier_lead,
-    insert_dt,
-    update_dt
-)
-SELECT
-    0,
-    -1,
-    'UNKNOWN',
-    'Unknown Product',
-    -1,
-    'Unknown Category',
-    0.00,
-    -1,
-    'UNKNOWN',
-    -1,
-    CURRENT_DATE,
-    CURRENT_DATE
-WHERE NOT EXISTS (
-    SELECT 1 FROM BL_DM.DIM_PRODUCT_SUPPLIER
-    WHERE product_surr_id = 0
-);
-
-COMMIT;
-
 ------------------------------------------------------------------------------
 -- DIM_STORES
 -- This table stores details about stores
 
 CREATE TABLE IF NOT EXISTS BL_DM.DIM_STORES (
-    store_surr_id   BIGINT      PRIMARY KEY,  ---- surrogate key
+    store_surr_id   BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  ---- surrogate key
     store_id        BIGINT,  ---- natural key
     store_src_id    VARCHAR(100),
     store_location  VARCHAR(50),
@@ -129,31 +57,6 @@ CREATE TABLE IF NOT EXISTS BL_DM.DIM_STORES (
     insert_dt       DATE,
     update_dt       DATE
 );
-
--- Default row
-INSERT INTO BL_DM.DIM_STORES (
-    store_surr_id,
-    store_id,
-    store_src_id,
-    store_location,
-    state,
-    insert_dt,
-    update_dt
-)
-SELECT
-    0,
-    -1,
-    'UNKNOWN',
-    'Unknown Store',
-    '??',
-    CURRENT_DATE,
-    CURRENT_DATE
-WHERE NOT EXISTS (
-    SELECT 1 FROM BL_DM.DIM_STORES
-    WHERE store_surr_id = 0
-);
-
-COMMIT;
 
 -----------------------------------------------------------------------------
 -- DIM_DATES
@@ -169,43 +72,18 @@ CREATE TABLE IF NOT EXISTS BL_DM.DIM_DATES (
     year         INT
 );
 
-COMMIT;
-
 ------------------------------------------------------------------------------
 -- DIM_PROMOTION
 -- This table stores details about promotions
 
 CREATE TABLE IF NOT EXISTS BL_DM.DIM_PROMOTION (
-    promotion_surr_id  BIGINT       PRIMARY KEY, ---- surrogate key
+    promotion_surr_id  BIGINT       GENERATED ALWAYS AS IDENTITY PRIMARY KEY, ---- surrogate key
     promotion_id       BIGINT, ---- natural key
     promotion_src_id   VARCHAR(100),
     promotion_applied  VARCHAR(50),
     insert_dt          DATE,
     update_dt          DATE
 );
-
--- Default row
-INSERT INTO BL_DM.DIM_PROMOTION (
-    promotion_surr_id,
-    promotion_id,
-    promotion_src_id,
-    promotion_applied,
-    insert_dt,
-    update_dt
-)
-SELECT
-    0,
-    -1,
-    'UNKNOWN',
-    'Unknown Promo',
-    CURRENT_DATE,
-    CURRENT_DATE
-WHERE NOT EXISTS (
-    SELECT 1 FROM BL_DM.DIM_PROMOTION
-    WHERE promotion_surr_id = 0
-);
-
-COMMIT;
 
 ------------------------------------------------------------------------------
 -- FCT_SALES_DD
@@ -245,8 +123,124 @@ CREATE TABLE IF NOT EXISTS BL_DM.FCT_SALES_DD (
        FOREIGN KEY (promotion_surr_id)
        REFERENCES BL_DM.DIM_PROMOTION(promotion_surr_id)
 );
-
-
--- Commit the transaction
+-----Commit DDL changes
 COMMIT;
 
+-- Start DML transaction
+BEGIN;
+-- Insert default row  for DIM_CUSTOMER_SCD
+INSERT INTO BL_DM.DIM_CUSTOMER_SCD (
+    customer_surr_id,
+    customer_id,
+    customer_src_id,
+    customer_name,
+    customer_age,
+    customer_level,
+    gender,
+    customer_income,
+    start_dt,
+    end_dt,
+    is_active,
+    insert_dt,
+    update_dt
+)
+SELECT
+    0,
+    -1,
+    'UNKNOWN',
+    'Unknown',
+    -1,
+    'Unknown',
+    'U',
+    -1,
+    '1900-01-01',
+    '1900-01-01',
+    'N',
+    CURRENT_DATE,
+    CURRENT_DATE
+WHERE NOT EXISTS (
+    SELECT 1 FROM BL_DM.DIM_CUSTOMER_SCD
+    WHERE customer_surr_id = 0
+);
+
+-- Default row for DIM_PRODUCT_SUPPLIER
+-----In this row represents an "Unknown" product and is used to handle cases where product data is missing
+INSERT INTO BL_DM.DIM_PRODUCT_SUPPLIER (
+    product_surr_id,  ---- surrogate key
+    product_id,  ---- natural key
+    product_src_id,
+    product_name,
+    category_id,
+    category_name,
+    unit_price,
+    supplier_id,
+    supplier_src_id,
+    supplier_lead,
+    insert_dt,
+    update_dt
+)
+SELECT
+    0,
+    -1,
+    'UNKNOWN',
+    'Unknown Product',
+    -1,
+    'Unknown Category',
+    0.00,
+    -1,
+    'UNKNOWN',
+    -1,
+    CURRENT_DATE,
+    CURRENT_DATE
+WHERE NOT EXISTS (
+    SELECT 1 FROM BL_DM.DIM_PRODUCT_SUPPLIER
+    WHERE product_surr_id = 0
+);
+
+
+-- Default row for DIM_STORES
+INSERT INTO BL_DM.DIM_STORES (
+    store_surr_id,
+    store_id,
+    store_src_id,
+    store_location,
+    state,
+    insert_dt,
+    update_dt
+)
+SELECT
+    0,
+    -1,
+    'UNKNOWN',
+    'Unknown Store',
+    '??',
+    CURRENT_DATE,
+    CURRENT_DATE
+WHERE NOT EXISTS (
+    SELECT 1 FROM BL_DM.DIM_STORES
+    WHERE store_surr_id = 0
+);
+
+-- Default row for DIM_PROMOTION
+INSERT INTO BL_DM.DIM_PROMOTION (
+    promotion_surr_id,
+    promotion_id,
+    promotion_src_id,
+    promotion_applied,
+    insert_dt,
+    update_dt
+)
+SELECT
+    0,
+    -1,
+    'UNKNOWN',
+    'Unknown Promo',
+    CURRENT_DATE,
+    CURRENT_DATE
+WHERE NOT EXISTS (
+    SELECT 1 FROM BL_DM.DIM_PROMOTION
+    WHERE promotion_surr_id = 0
+);
+
+-- Commit DML changes
+COMMIT;
